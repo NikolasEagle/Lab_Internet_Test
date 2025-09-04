0cc3180c308bdcfd2fc265b59290b11c696cf883
Nickolas <eagle.dev.stack@gmail.com>
2025-09-04 16:53:12 +0400

    Добавление логотипа

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,24 @@
 import styles from "./Header.module.scss";
 
 export default function Header() {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <div className={styles.roundes}>
+          <div
+            style={{ background: "rgba(42, 108, 234, 1)" }}
+            className={styles.round}
+          ></div>
+          <div
+            style={{
+              background: "rgba(236, 239, 242, 1)",
+              marginLeft: "-12px",
+            }}
+            className={styles.round}
+          ></div>
+        </div>
+        <h5>testLab</h5>
+      </div>
+    </header>
+  );
 }
