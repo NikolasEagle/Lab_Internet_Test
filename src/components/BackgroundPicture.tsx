3f494847bb796b46141d072f244a0335e45c6cf7
Nickolas <eagle.dev.stack@gmail.com>
2025-09-04 16:35:24 +0400

    Добавление описания и кнопки на главный фон

--- src/components/BackgroundPicture.tsx
+++ src/components/BackgroundPicture.tsx
@@ -1,5 +1,15 @@
 import styles from "./BackgroundPicture.module.scss";
 
 export default function BackgroundPicture() {
-  return <div className={styles.picture}></div>;
+  return (
+    <div className={styles.picture}>
+      <div className={styles.description_wrapper}>
+        <div className={styles.description}>
+          <h1>Говорят, никогда не поздно сменить профессию</h1>
+          <p>Сделай круто тестовое задание и у тебя получится</p>
+        </div>
+        <button>Проще простого!</button>
+      </div>
+    </div>
+  );
 }
