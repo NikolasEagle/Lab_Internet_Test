38f2143123c1737da4eeceb80d36f271d60059b1
Nickolas <eagle.dev.stack@gmail.com>
2025-09-04 13:25:12 +0400

    Добавление компонента фоновой картинки

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import BackgroundPicture from "./components/BackgroundPicture";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <BackgroundPicture />
     </>
   );
 }
---
+++ src/components/BackgroundPicture.tsx
@@ -0,0 +1,5 @@
+import styles from "./BackgroundPicture.module.scss";
+
+export default function BackgroundPicture() {
+  return <div className={styles.picture}></div>;
+}
